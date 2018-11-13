601daf67781aedad5d0c33adc6e31f2528de732f
Jun ho Lee <june20516@gmail.com>
2018-11-13 21:19:18 +0900

    백준 - for 문 쓰기 단계
    2741, 2742, 2739, 2438, 2439, 2440

---
+++ python/pointStar.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+def pointStar(N):
+    for i in range(N):
+        star = ""
+        for j in range(N-(i+1)):
+            star += " "
+        for k in range(i+1):
+            star += "*"
+        print(star)
+
+def pointStarReverse(N):
+    for i in range(N):
+        star = ""
+        for i in range(N-i):
+            star += "*"
+        print(star)
+
+
