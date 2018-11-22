e9bfa56a9aa23b9f7b3442efb264e76241f74faa
Jun ho Lee <june20516@gmail.com>
2018-11-22 17:10:37 +0900

    별찍기

--- python/star10.py
+++ python/star10.py
@@ -1,34 +1,71 @@
 import math
 
-s = ['  *   ',' * *  ','***** ']
+n = int(input())
+
+# arr = []
+
+# for i in range(n):
+#     arr.append([])
+#     for j in range(n):
+#         arr[i].append(' ')
+#     arr[i].append('\n')
+
+# def starSq(n, x, y):
+#     if n == 3:
+#         for i in range(3):
+#             for j in range(3):
+#                 arr[y+i][x+j] = '*'
+#         arr[y+1][x+1] = ' '
+#         return
+#     elif n == 1:
+#         arr[0][0] = '*'
+#         return
+#     starSq(int(n/3),x,y)
+#     starSq(int(n/3),x+int(n/3),y)
+#     starSq(int(n/3),x+2*int(n/3),y)
+#     starSq(int(n/3),x,y+int(n/3))
+#     starSq(int(n/3),x+2*int(n/3),y+int(n/3))
+#     starSq(int(n/3),x,y+2*int(n/3))
+#     starSq(int(n/3),x+int(n/3),y+2*int(n/3))
+#     starSq(int(n/3),x+2*int(n/3),y+2*int(n/3))
+
+# starSq(n,0,0)
+
+# for i in range(n):
+#     for j in range(n+1):
+#         print(arr[i][j],end='')
+
+
+# n = 1
+# a1 = ['*']
+
+# n = 3
+# a3 = [a1[0]*3, a1[0]+' '+a1[0], a1[0]*3]
+
+# n = 9
+# a9 = [a3[0]*3, a3[1]*3, a3[2]*3, a3[0]+' '*3+a3[0],a3[0]+' '*3+a3[0],a3[0]+' '*3+a3[0], a3[0]*3, a3[1]*3, a3[2]*3]
+
+# n = 27
+
+# a27 = [a9*3]
 
 
 
-def make_stars(shift):
+a = ['*']
 
-    for i in range(len(s)):
+def line(n,lst):
+    end = lst * 3
+    mid = []
+    for i in range(int(n/3)):
+        mid.append([lst[i], '{}'.format(' '*int(n/3)), lst[i]])
+    return [end,mid,end]
 
-        s.append(s[i]+s[i])
+arr = ['*']
+idx = [1]
+for i in range(8):
+    idx.append(idx[i]*3)
 
-        s[i] = ' '*shift+s[i]+' '*shift
+for i in range(1,idx.index(n)+1,+1):
+    arr = line(idx[i], arr)
 
-
-
-numb = int(input())
-
-k = int(math.log(numb/3,2))
-
-
-
-for i in range(k):
-
-    make_stars(2**i*3)
-
-    
-
-for i in range(len(s)):
-
-    print(s[i])
-
-
-print("a")
+print(arr)
